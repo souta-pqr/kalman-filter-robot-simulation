b97a12c72370c6f0356dae617bc9f8cefe5c58bb
souta-pqr <boriko0116@gmail.com>
2025-12-19 15:09:46 +0900

    tests/test_robot_simulator.pyの修正

--- tests/test_robot_simulator.py
+++ tests/test_robot_simulator.py
@@ -31,6 +31,17 @@ def test_robot1d_observe():
     assert obs == 5.0
 
 
+def test_robot1d_get_position():
+    """Test 1D robot get_position method"""
+    robot = Robot1D(initial_position=3.5, process_noise_std=0.0, observation_noise_std=0.0)
+    true_pos = robot.get_position()
+    assert true_pos == 3.5
+    
+    # Position should not change by getting it
+    robot.move(distance=2.0)
+    assert robot.get_position() == 5.5
+
+
 def test_robot2d_initialization():
     """Test 2D robot initialization"""
     robot = Robot2D(
@@ -85,3 +96,50 @@ def test_robot2d_rotation():
     assert np.isclose(robot.state[0], 0.0, atol=1e-5)
     assert np.isclose(robot.state[1], 0.0, atol=1e-5)
     assert np.isclose(robot.state[2], np.pi / 2, atol=1e-5)
+
+
+def test_robot2d_get_state():
+    """Test 2D robot get_state method"""
+    robot = Robot2D(
+        initial_state=[1.0, 2.0, 0.5],
+        process_noise_std=[0.0, 0.0, 0.0],
+        observation_noise_std=[0.0, 0.0, 0.0],
+    )
+    
+    state = robot.get_state()
+    
+    # Should return a copy
+    assert np.allclose(state, [1.0, 2.0, 0.5])
+    assert state is not robot.state
+    
+    # Modifying returned state should not affect robot
+    state[0] = 999.0
+    assert robot.state[0] == 1.0
+
+
+def test_robot2d_angle_normalization():
+    """Test angle normalization in 2D robot"""
+    robot = Robot2D(
+        initial_state=[0.0, 0.0, 0.0],
+        process_noise_std=[0.0, 0.0, 0.0],
+        observation_noise_std=[0.0, 0.0, 0.0],
+    )
+    
+    # Test angle wrapping above pi
+    robot.state[2] = 4 * np.pi
+    robot.move(control_input=[0.0, 0.0])  # Triggers normalization
+    assert -np.pi <= robot.state[2] <= np.pi
+    
+    # Test angle wrapping below -pi
+    robot.state[2] = -3.5 * np.pi
+    robot.move(control_input=[0.0, 0.0])
+    assert -np.pi <= robot.state[2] <= np.pi
+    
+    # Test exact boundaries
+    robot.state[2] = np.pi
+    robot.move(control_input=[0.0, 0.0])
+    assert -np.pi <= robot.state[2] <= np.pi
+    
+    robot.state[2] = -np.pi
+    robot.move(control_input=[0.0, 0.0])
+    assert -np.pi <= robot.state[2] <= np.pi
